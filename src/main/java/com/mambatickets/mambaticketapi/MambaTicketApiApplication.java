14e7cca37d34e5f7d5941acb99c4166d8dd80507
Hans Schedel <hans.haugusth@gmail.com>
2020-03-11 03:12:58 -0300

    First configuration

---
+++ src/main/java/com/mambatickets/mambaticketapi/MambaTicketApiApplication.java
@@ -0,0 +1,13 @@
+package com.mambatickets.mambaticketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MambaTicketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MambaTicketApiApplication.class, args);
+	}
+
+}
