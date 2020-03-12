542e08cdca22dd7aef75114cd47c6822f0f19f48
Hans Schedel <hans.haugusth@gmail.com>
2020-03-12 00:09:23 -0300

    Add swagger config

--- src/main/java/com/mambatickets/mambaticketapi/SwaggerConfig.java
+++ src/main/java/com/mambatickets/mambaticketapi/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.mambatickets.mambaticketapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +19,17 @@ public class SwaggerConfig {
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
+            .build()
+            .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+            .title("Mamba Ticket REST API")
+            .description("Mamba Ticket REST API")
+            .version("1.0.0")
+            .license("Apache License Version 2.0")
+            .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
             .build();
     }
 }
